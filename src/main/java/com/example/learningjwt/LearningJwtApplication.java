ede55b94f23a61e78cf4a3e141cdeba5a83e3874
Sohyun Wi <sohyun.wi@gmail.com>
2024-10-22 16:59:54 +0900

    Jwt 학습용 프로젝트 첫번재 커밋

---
+++ src/main/java/com/example/learningjwt/LearningJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningjwt/LearningJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
